cd7c8f3c7ca87d903145203e6a05ca80cfcbf32e
rupam0011 <rupam001majumder@gmail.com>
2025-03-25 19:37:23 +0530

    stack updated

--- src/app/stack/Stack.jsx
+++ src/app/stack/Stack.jsx
@@ -10,30 +10,6 @@ import bootstrap from '../../../assets/bootstrap.png'
 import git from '../../../assets/git.png'
 import Image from 'next/image'
 import { client } from '@/sanity/lib/client'
-// import { metadata } from '../layout'
-
-
-// export const metadata = {
-//   title: "Stack"
-// }
-// export const generateMetadata = () => ({
-//   title: "Stack",
-//   description: "Frameworks, libraries, and tools I use regularly in my projects.",
-//   keywords: "React, Redux, Material UI, Bootstrap",
-// });
-
-// const techStack = [
-//   { name: "VS Code", desc: "Code Editor", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
-//   { name: "React Js", desc: "Frontend Library", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" },
-//   { name: "CSS", desc: "For Styling the Webpage", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" },
-//   { name: "Redux", desc: "State Management", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/redux/redux-original.svg" },
-//   { name: "Material UI", desc: "Component Library", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg" },
-//   { name: "Node Js", desc: "JavaScript Runtime", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg" },
-//   { name: "Bootstrap", desc: "Component Library", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" },
-//   { name: "JavaScript", desc: "Making the Logic", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" },
-//   { name: "Git", desc: "Version Control System", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" },
-//   { name: "HTML", desc: "Structure the Webpage", icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" }
-// ];
 
 
 
